b1789b69b2082af96b342ea79e61a9c1e5a0d692
Bpeen Gurung <bipin67678grg@gmail.com>
2022-08-16 15:35:53 +0545

    short array find greatest num

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+// Write a function that takes in an array and sort the arr\
+const  sortArray = (array) =>{
+    let greatest = [0] ;
+
+    //Loping array
+    for (const number of array){
+        //console.log(number);
+        if(number > greatest){
+            greatest = number
+        }
+    }
+    return greatest
+}
+console.log(sortArray([1,2,3,4,5,6]))
