74f57a6dd16354f5b891efec53f3148331d748be
Bipin Gurung <bipin67678grg@gmail.com>
2022-08-18 22:57:50 +0545

    check letter frequency in js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ const  sortArray = (array) =>{
     return greatest
 }
 console.log(sortArray([1,2,3,4,5,6]))
+
+const letterFrequency = (phrase) => {
+  // letterFrequency('haha') 👉 {'h': 2, 'a': 2}
+  console.log(phrase)
+  // make a `frequency` object {}
+  let frequency = {}
+  for (const letter of phrase) {
+    // check if letter exists in frequency
+    if (letter in frequency) {
+      // increment the value by +1
+      frequency[letter] += 1
+      // otherwise, set the value to 1
+    } else {
+      frequency[letter] = 1
+    }
+  }
+  return frequency
+}
+
+// console.log(letterFrequency('lol, what are you doing later tonight lol, haha!'))
